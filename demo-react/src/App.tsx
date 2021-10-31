00ad2548d1f917d76bbd1d2420a2d096821f4b7d
Miguel Iglesias <miguel.iglesias@dimeder.com>
2021-10-31 11:17:59 -0400

    getting there

--- demo-react/src/App.tsx
+++ demo-react/src/App.tsx
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface CoffeeShop{
+    id: number;
+    name: string;
+    address: string;
+    internetReliability: number;
+    phone: string;
+    powerAccessible: boolean;
+    priceOfCoffee: number;
+
 }
+
+interface State {
+    isLoading: boolean;
+    coffeeShops: CoffeeShop[];
+}
+
+class App extends React.Component {
+  state: State = {
+    isLoading: true,
+    coffeeShops: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/api/coffeeshops');
+    const body = await response.json();
+    this.setState({coffeeShops: body._embedded.coffeeshops, isLoading: false});
+  }
+
+  render() {
+    const {coffeeShops, isLoading} = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="App-intro">
+            <h2>Coffee Shop List</h2>
+            {coffeeShops.map(coffeeShop =>
+              <div key={coffeeShop.id}>
+                {coffeeShop.name} - {coffeeShop.address}
+              </div>
+            )}
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
 
 export default App;
